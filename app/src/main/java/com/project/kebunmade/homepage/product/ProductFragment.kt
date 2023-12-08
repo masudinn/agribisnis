1ecabcf8482f3e4b414066159d9caf1fcd8cd39c
masudin <fahrudinefendi2087@gmail.com>
2023-12-08 13:41:17 +0700

    dashboard static

--- app/src/main/java/com/project/kebunmade/homepage/product/ProductFragment.kt
+++ app/src/main/java/com/project/kebunmade/homepage/product/ProductFragment.kt
@@ -33,38 +33,38 @@ class ProductFragment : Fragment() {
 
     override fun onResume() {
         super.onResume()
-        showCategory()
-        initLatestProduct()
+//        showCategory()
+//        initLatestProduct()
     }
 
-    private fun initLatestProduct() {
-        val layoutManager = LinearLayoutManager(
-            activity,
-            LinearLayoutManager.HORIZONTAL,
-            false
-        )
-        layoutManager.reverseLayout = true
-        layoutManager.stackFromEnd = true
-        binding?.newProductRv?.layoutManager = layoutManager
-
-        productAdapter = ProductFragmentAdapter()
-        binding?.newProductRv?.adapter = productAdapter
-
-        val viewModel = ViewModelProvider(this)[ProductViewModel::class.java]
-
-        binding?.progressBarNewProduct?.visibility = View.VISIBLE
-        viewModel.setListProductAll()
-        viewModel.getProductList().observe(viewLifecycleOwner) { product ->
-            if (product.size > 0) {
-                binding?.progressBarNewProduct?.visibility = View.GONE
-                productAdapter!!.setData(product)
-                binding?.noDataNewProduct?.visibility = View.GONE
-            } else {
-                binding?.progressBarNewProduct?.visibility = View.GONE
-                binding?.noDataNewProduct?.visibility = View.VISIBLE
-            }
-        }
-    }
+//    private fun initLatestProduct() {
+//        val layoutManager = LinearLayoutManager(
+//            activity,
+//            LinearLayoutManager.HORIZONTAL,
+//            false
+//        )
+//        layoutManager.reverseLayout = true
+//        layoutManager.stackFromEnd = true
+//        binding?.newProductRv?.layoutManager = layoutManager
+//
+//        productAdapter = ProductFragmentAdapter()
+//        binding?.newProductRv?.adapter = productAdapter
+//
+//        val viewModel = ViewModelProvider(this)[ProductViewModel::class.java]
+//
+//        binding?.progressBarNewProduct?.visibility = View.VISIBLE
+//        viewModel.setListProductAll()
+//        viewModel.getProductList().observe(viewLifecycleOwner) { product ->
+//            if (product.size > 0) {
+//                binding?.progressBarNewProduct?.visibility = View.GONE
+//                productAdapter!!.setData(product)
+//                binding?.noDataNewProduct?.visibility = View.GONE
+//            } else {
+//                binding?.progressBarNewProduct?.visibility = View.GONE
+//                binding?.noDataNewProduct?.visibility = View.VISIBLE
+//            }
+//        }
+//    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -73,47 +73,47 @@ class ProductFragment : Fragment() {
         // Inflate the layout for this fragment
         binding = FragmentProductBinding.inflate(inflater, container, false)
 
-        checkRole()
+//        checkRole()
         showOnboardingImage()
 
         return binding?.root
     }
 
-    private fun checkRole() {
-        val uid = FirebaseAuth.getInstance().currentUser!!.uid
-        FirebaseFirestore
-            .getInstance()
-            .collection("users")
-            .document(uid)
-            .get()
-            .addOnSuccessListener {
-                role = "" + it.data?.get("role")
-                if(role == "admin") {
-                    binding?.categoryAdd?.visibility = View.VISIBLE
-                }
-            }
-    }
+//    private fun checkRole() {
+//        val uid = FirebaseAuth.getInstance().currentUser!!.uid
+//        FirebaseFirestore
+//            .getInstance()
+//            .collection("users")
+//            .document(uid)
+//            .get()
+//            .addOnSuccessListener {
+//                role = "" + it.data?.get("role")
+//                if(role == "admin") {
+//                    binding?.categoryAdd?.visibility = View.VISIBLE
+//                }
+//            }
+//    }
 
-    private fun showCategory() {
-        binding?.categoryRv?.layoutManager = LinearLayoutManager(
-            activity,
-            LinearLayoutManager.HORIZONTAL,
-            false
-        )
-        categoryAdapter = CategoryAdapter()
-        binding?.categoryRv?.adapter = categoryAdapter
-
-        val viewModel = ViewModelProvider(this)[CategoryViewModel::class.java]
-
-        binding?.categoryPb?.visibility = View.VISIBLE
-        viewModel.setListCategory()
-        viewModel.getCategoryList().observe(viewLifecycleOwner) { category ->
-            if (category.size > 0) {
-                categoryAdapter!!.setData(category)
-            }
-            binding?.categoryPb?.visibility = View.GONE
-        }
-    }
+//    private fun showCategory() {
+//        binding?.categoryRv?.layoutManager = LinearLayoutManager(
+//            activity,
+//            LinearLayoutManager.HORIZONTAL,
+//            false
+//        )
+//        categoryAdapter = CategoryAdapter()
+//        binding?.categoryRv?.adapter = categoryAdapter
+//
+//        val viewModel = ViewModelProvider(this)[CategoryViewModel::class.java]
+//
+//        binding?.categoryPb?.visibility = View.VISIBLE
+//        viewModel.setListCategory()
+//        viewModel.getCategoryList().observe(viewLifecycleOwner) { category ->
+//            if (category.size > 0) {
+//                categoryAdapter!!.setData(category)
+//            }
+//            binding?.categoryPb?.visibility = View.GONE
+//        }
+//    }
 
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
